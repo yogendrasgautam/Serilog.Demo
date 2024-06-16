74f3e7697bce6f72272496d843f4389e86213daf
yogi <yogendrasgautam@gmail.com>
2024-06-16 14:19:49 +0530

    updated serilog config

--- Serilog.Demo/Program.cs
+++ Serilog.Demo/Program.cs
@@ -17,8 +17,16 @@ namespace Serilog.Demo
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            // Add Logger
+            // Add Logger without adding configuration in appsettings.json
             builder.Host.UseSerilog((context, configuration) => configuration
+                            .MinimumLevel.Warning()
+                            .MinimumLevel.Override("Microsoft", LogEventLevel.Fatal)
+                            .MinimumLevel.Override("System", LogEventLevel.Fatal)
+                            .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}",
+                                                theme: SystemConsoleTheme.Colored)
+                            .WriteTo.File("logs/log.txt",
+                                            outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}", rollingInterval: RollingInterval.Day, 
+                                            rollOnFileSizeLimit: true, fileSizeLimitBytes: 500000)
                             .WriteTo.Seq("http://localhost:5341"));
 
             var app = builder.Build();
